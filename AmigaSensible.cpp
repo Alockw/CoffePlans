dc0002d2152984dee3359422e5776023209efb30
lauraippolito28 <159057618+lauraippolito28@users.noreply.github.com>
2024-04-18 17:57:14 -0500

    Update AmigaSensible.cpp

--- AmigaSensible.cpp
+++ AmigaSensible.cpp
@@ -1,3 +1,4 @@
+// La amiga sensible es aquella amiga que tiene los lugares establecidos que quiere visitar, no hay mucho orden con respecto a las horas pero quiere visitar cada destino que tiene dentro de su lista de planes
 #include <iostream> 
 #include <string> 
 #include <set> 
@@ -6,7 +7,8 @@
 using namespace std; 
 
 int main(){
-    // Conjunto de países de destino
+
+    // Conjunto de ciudades de destino
     set<string> destinos; 
 
     destinos.insert("Oslo");
@@ -20,38 +22,52 @@ int main(){
         cout << destino << endl; 
     }
 
+
     // Mapa de lugares visitados en cada país
-    map <int, string> oslo;
-    oslo[1]="Parque Oslofjord";
-    oslo[2]="Botanical Garden";
-    oslo[3]="Lago Sognsvann";
-    for(const auto& par: oslo)
+
+    map <int, string> itinerarioOslo;
+    // dia 1
+    itinerarioOslo[1]="Parque Oslofjord";
+    // dia 2 
+    itinerarioOslo[2]="Botanical Garden";
+    // dia 3
+    itinerarioOslo[3]="Lago Sognsvann";
+    for(const auto& par: itinerarioOslo)
     {
         int orden = par.first();
         string lugarAVisitar = par.second();
         cout << "El" << orden << "lugar a visitar es: " << lugarAVisitar << endl;
     }
 
-    map <int, string> estocolmo;
-    estocolmo[1]="Rosendals Garden";
-    estocolmo[2]="Riddarholmen";
-    estocolmo[3]="Porjus";
-    for(const auto& par: estocolmo)
+
+    map <int, string> itinerarioEstocolmo;
+    // dia 4
+    itinerarioEstocolmo[1]="Rosendals Garden";
+    // dia 5
+    itinerarioEstocolmo[2]="Riddarholmen";
+    // dia 6
+    itinerarioEstocolmo[3]="Porjus";
+    for(const auto& par: itinerarioEstocolmo)
     {
         int orden = par.first();
         string lugarAVisitar = par.second();
         cout << "El" << orden << "lugar a visitar es: " << lugarAVisitar << endl;
     }
 
-    map <int, string> helsinki;
-    helsinki[1]="Parque Sibelius";
-    helsinki[2]="Playa Aurikolahti";
-    helsinki[3]="Kumpula Botanic Garden";
-    for(const auto& par: helsinki)
+
+    map <int, string> itinerarioHelsinki;
+    // dia 7
+    itinerarioHelsinki[1]="Parque Sibelius";
+    // dia 8 
+    itinerarioHelsinki[2]="Playa Aurikolahti";
+    // dia 9 
+    itinerarioHelsinki[3]="Kumpula Botanic Garden";
+    for(const auto& par: itinerarioHelsinki)
     {
         int orden = par.first();
         string lugarAVisitar = par.second();
         cout << "El" << orden << "lugar a visitar es: " << lugarAVisitar << endl;
     }
+    
     return 0;
 };
